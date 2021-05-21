9eb0429827cd33b2818ee5b2a1f6a25f74e36e9c
eomjeongjae <jeongjae.eom@gmail.com>
2021-05-21 10:52:16 +0900

    test code push

---
+++ my-test/css-test/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "./Button2";
+import Box from "./Box2";
+
+function App() {
+  return (
+      <div>
+        <Button size="big" />
+        <Button size="small" />
+        <Box size="big" />
+        <Box size="small" />
+      </div>
+  );
+}
+
+export default App;
